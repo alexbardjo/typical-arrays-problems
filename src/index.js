bdc2a61d2d928b744449f308c22f29067844e66a
Alexbardjo <alexbardjo@gmail.com>
2021-03-02 21:05:53 +0200

    feat: implement 3 functions: max, min, and avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
+exports.min = function min(array) {
+    if (min.arguments.length === 0 || !(array.length) || array === Infinity) {
+        return false;
+    } else {
+        // const arr = Array.from(arguments);
+        return Math.min(...array);
+    }
 
-exports.min = function min (array) {
-  return 0;
-}
+};
 
-exports.max = function max (array) {
-  return 0;
-}
+exports.max = function max(array) {
+    if (max.arguments.length === 0 || !(array.length) || array === Infinity) {
+        return false;
+    } else {
+        // const arr = Array.from(arguments);
+        return Math.max(...array);
+    }
+};
 
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.avg = function avg(array) {
+    if (avg.arguments.length === 0 || !(array.length) || array === Infinity) {
+        return false;
+    } else {
+        // const arr = Array.from(arguments);
+        const sum = array.reduce((accumulator, currentValue) => accumulator + currentValue);
+        return sum / array.length;
+    }
+};
